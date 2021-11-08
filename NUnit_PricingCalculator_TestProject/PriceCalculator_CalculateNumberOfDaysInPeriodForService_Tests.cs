fe876c080516ce06632861cce2da9fb7cc70bbb1
Oscar Andersson <oscarandersson2@gmail.com>
2021-11-08 11:56:59 +0100

    Update av test

---
+++ NUnit_PricingCalculator_TestProject/PriceCalculator_CalculateNumberOfDaysInPeriodForService_Tests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using PricingCalculator.Services;
+
+namespace NUnit_PricingCalculator_TestProject
+{
+    public class PriceCalculator_CalculateNumberOfDaysInPeriodForService_Tests : PricingCalculator_TestBase
+    {
+        [OneTimeSetUp]
+        public void TestSetup()
+        {
+            var test = this.BuildConfiguration(TestContext.CurrentContext.TestDirectory);
+            this.m_PriceCalculateService = new PriceCalculateService(this.BuildConfiguration(TestContext.CurrentContext.TestDirectory));
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+        #region Test av metoden CalculateNumberOfDaysInPeriodForService
+
+        // TODO
+
+        #endregion // End of region Test av metoden CalculateNumberOfDaysInPeriodForService
+    }
+}
